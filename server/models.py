5b42493b628b7baca4c34fd6ee4dddffb5d526e1
Brett de Bear <bkdebear@gmail.com>
2023-04-05 08:36:31 -0700

    added assets folder to store photos

--- server/models.py
+++ server/models.py
@@ -26,5 +26,15 @@ class User(db.Model, SerializerMixin):
 
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+    
+class Park(db.Model, SerializerMixin):
+    __tablename__ = 'parks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    location = db.Column(db.String)
+    description = db.Column(db.String)
+    image = db.Column(db.String)
+
 
     
